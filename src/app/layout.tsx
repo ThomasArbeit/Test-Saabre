7ae4997482d4f8c9b0002ff3da151d6ead4ec210
Thomas Arbeit <tarbeit@hotmail.fr>
2025-04-28 15:11:10 +0200

    added fav icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Catalogue des Voitures Électrifiées : Voitures Électriques, Hybrides et Plug-in Hybrides",
   description: "Découvrez notre catalogue complet des véhicules électrifiés, incluant les voitures électriques, hybrides et hybrides rechargeables disponibles sur le marché. Parcourez les modèles de manière claire et structurée pour trouver le véhicule qui répond à vos besoins écologiques et technologiques.",
+  icons: {
+    icon: '/carKnk.svg',
+  },
 };
 
 export default function RootLayout({
